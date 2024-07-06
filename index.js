9aa1342e4eb85e7337c67d38dd89987da0bd262b
Folze <thefolze@gmail.com>
2024-07-06 14:05:33 +0500

    Первый коммит

---
+++ index.js
@@ -0,0 +1,62 @@
+import {
+  differenceInYears,
+  differenceInMonths,
+  differenceInDays,
+  differenceInHours,
+  differenceInMinutes,
+  differenceInSeconds,
+} from "date-fns";
+import { se } from "date-fns/locale";
+
+const getElement = (element) => document.querySelector(element);
+const UI_ELEMENTS = {
+  countdownDate: getElement(".countdown-date"),
+  countdownDateInput: getElement(".countdown-date__input"),
+  countdownDateButton: getElement(".countdown-date__button"),
+  timeRemaining: getElement(".time-remaining"),
+};
+
+function calculateDifference(startDate, endDate) {
+  const yearsDifference = differenceInYears(startDate, endDate);
+  const monthsDifference = differenceInMonths(startDate, endDate) % 12;
+  const daysDifference = differenceInDays(startDate, endDate) % 30;
+  const hoursDifference = differenceInHours(startDate, endDate) % 24;
+  const minutesDifference = differenceInMinutes(startDate, endDate) % 60;
+  const secondsDifference = differenceInSeconds(startDate, endDate) % 60;
+  return {
+    yearsDifference,
+    monthsDifference,
+    daysDifference,
+    hoursDifference,
+    minutesDifference,
+    secondsDifference,
+  };
+}
+
+let interval;
+
+const updateDisplay = (timeDiff) => {
+  if (timeDiff.yearsDifference == 0) {
+    UI_ELEMENTS.timeRemaining.textContent = `
+  ${timeDiff.monthsDifference} месяцев
+  ${timeDiff.daysDifference} дней ${timeDiff.hoursDifference} часов
+  ${timeDiff.minutesDifference} минут ${timeDiff.secondsDifference} секунд`;
+  } else {
+    UI_ELEMENTS.timeRemaining.textContent = `
+    ${timeDiff.yearsDifference} лет ${timeDiff.monthsDifference} месяцев
+    ${timeDiff.daysDifference} дней ${timeDiff.hoursDifference} часов
+    ${timeDiff.minutesDifference} минут ${timeDiff.secondsDifference} секунд`;
+  }
+};
+
+UI_ELEMENTS.countdownDate.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const selectedDate = new Date(UI_ELEMENTS.countdownDateInput.value);
+  clearInterval(interval);
+
+  interval = setInterval(() => {
+    const now = new Date();
+    const timeDiff = calculateDifference(selectedDate, now);
+    updateDisplay(timeDiff);
+  }, 1000);
+});
